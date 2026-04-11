4fb6abf04181aa327bfb226d1a6685aeaf7c11fb
kevinrato17 <kevinrato17@gmail.com>
2026-04-11 22:15:53 +0100

    Add Google Analytics integration to the application for enhanced tracking and analytics capabilities.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
+import Script from "next/script";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-YM55X0LJQJ"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-YM55X0LJQJ');
+  `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
