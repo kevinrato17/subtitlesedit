bf6f0df990e0e75709582da84c478c8f6c259254
kevinrato17 <kevinrato17@gmail.com>
2026-04-29 19:57:06 +0100

    Update Next.js configuration to disable trailing slashes for improved URL handling

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: false,
   transpilePackages: ["next-mdx-remote"],
   images: {
     remotePatterns: [
